55919403cec185b3b9a79debc16ef226a6c33348
sibeesans <105978585+sibeesans@users.noreply.github.com>
2024-01-01 07:44:32 +0700

    Update lekasi.sh

--- lekasi.sh
+++ lekasi.sh
@@ -796,7 +796,7 @@ print_success "All Packet"
 function menu(){
     clear
     print_install "Memasang Menu Packet"
-    wget ${REPO}menu/menu.zip
+    wget ${REPO}files/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
